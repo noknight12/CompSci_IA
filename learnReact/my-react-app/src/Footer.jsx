4ecaeb01b55f65e313f084ef76ae51bf0119d8b1
Karl <karlonasa12@gmail.com>
2024-07-03 00:50:54 -0700

    hi

---
+++ learnReact/my-react-app/src/Footer.jsx
@@ -0,0 +1,10 @@
+
+function Footer(){
+    return(
+        <footer>
+            <p>copyright</p>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ learnReact/my-react-app/src/Header.jsx
@@ -0,0 +1,19 @@
+function Header(){
+
+return(
+    <header>
+        <h1>my website</h1>
+        <nav>
+            <ul>
+                <li><a href="#"></a>Home</li>
+                <li><a href="#"></a>About</li>
+                <li><a href="#"></a>Services</li>
+                <li><a href="#"></a>Contacts</li>
+            </ul>
+        </nav>
+        <hr></hr>
+    </header>
+);
+}
+
+export default Header
