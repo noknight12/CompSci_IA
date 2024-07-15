7c013a2eb18ada333048992996e264ef0e21b1b6
Karl <karlonasa12@gmail.com>
2024-07-15 11:23:09 -0700

    update3

--- learnReact/my-react-app/DBManager.cjs
+++ learnReact/my-react-app/DBManager.cjs
@@ -8,18 +8,18 @@
 //db.run("DROP TABLE users")
 
 
-alert("gfj")
+
         
         const sqlite3 = require('sqlite3').verbose();
         let sql;
-    alert("fmd")
+ 
         const db = new sqlite3.Database('./Data.db', sqlite3.OPEN_READWRITE, (err)=>{
             if(err) return console.error(err.message);
             });
            
        //  sql = 'CREATE TABLE users(id INTEGER PRIMARY KEY, first_name,last_name,username,password,email)';
        // db.run(sql);
-         db.run("INSERT INTO users(id, first_name,last_name,username,password,email) VALUES(2, 'Ryan', 'Reynolds', 'jojo', 'grate', 'bob@gmail.com')")
+         db.run("INSERT INTO users(id, first_name,last_name,username,password,email) VALUES(7, 'Ryan', 'Reynolds', 'jojo', 'grate', 'bob@gmail.com')")
          
             
    
