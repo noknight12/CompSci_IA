4d9e79bd8dbb6a116bfbafd6d217ee0e6ff92c7f
noknight12 <karlonasa12@gmail.com>
2024-11-12 22:48:51 -0800

    hi

---
+++ learnReact/my-react-app/src/ContactUs.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+class ContactUs extends React.Component{
+
+
+    render(){
+
+        <>
+        <h>ContactUs</h>
+        </>
+    }
+}
+
+export default ContactUs
